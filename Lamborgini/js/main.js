626d69c0c72a54d88c9b88a97ef16828434abb15
Vitalya-Str <strela2789@gmail.com>
2023-03-02 00:09:35 +0600

    blog, footer

--- Lamborgini/js/main.js
+++ Lamborgini/js/main.js
@@ -3,17 +3,23 @@ const burgerBtn = document.querySelector(".burger-btn");
 
 burgerBtn.addEventListener("click", () => {
   menuMobile.classList.toggle("active");
-
 });
 
 const swiper = new Swiper(".swiper", {
   loop: true,
-  slidesPerView: 2,
-  spaceBetween: 20,
 
   navigation: {
     nextEl: ".swiper-button-right",
     prevEl: ".swiper-button-left",
+  },
+  breakpoints: {
+    0: {
+      slidesPerView: 1,
+    },
+    775: {
+      slidesPerView: 2,
+      spaceBetween: 20,
+    },
   },
 });
 
