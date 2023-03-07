78aa806c5d6f0a219dd2086043ac11bd44dbee35
Vitalya-Str <strela2789@gmail.com>
2023-03-07 19:58:50 +0600

    gulp

--- Logo/gulpfile.js
+++ Logo/gulpfile.js
@@ -9,11 +9,15 @@ global.app = {
 
 import { copy } from "./gulp/tasks/copy.js";
 import { reset } from "./gulp/tasks/reset.js";
+import { html } from "./gulp/tasks/html.js";
 
 function watcher() {
   gulp.watch(path.watch.files, copy);
+  gulp.watch(path.watch.html, html);
 }
 
-const dev = gulp.series(reset, copy, watcher);
+const mainTasks = gulp.parallel(copy, html);
+
+const dev = gulp.series(reset, mainTasks, watcher);
 
 gulp.task("default", dev);
