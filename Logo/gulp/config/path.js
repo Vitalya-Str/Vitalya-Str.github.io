2deb560d76089a599be35275adf6110f3bc55679
Vitalya-Str <strela2789@gmail.com>
2023-03-11 10:32:31 +0600

    images

--- Logo/gulp/config/path.js
+++ Logo/gulp/config/path.js
@@ -6,15 +6,25 @@ const srcFolder = `./src`;
 
 export const path = {
   build: {
+    js: `${buildFolder}/js/`,
+    css: `${buildFolder}/css/`,
     html: `${buildFolder}/`,
+    images: `${buildFolder}/img/`,
     files: `${buildFolder}/files/`,
   },
   src: {
+    js: `${srcFolder}/js/app.js`,
+    images: `${srcFolder}/img/**/*.{jpg,jpeg,png,gif,webp}`,
+    svg: `${srcFolder}/img/**/*.svg`,
+    scss: `${srcFolder}/scss/style.scss`,
     html: `${srcFolder}/*.html`,
     files: `${srcFolder}/files/**/*.*`,
   },
   watch: {
-    html: `${buildFolder}/**/*.html `,
+    js: `${srcFolder}/js/**/*.js`,
+    scss: `${srcFolder}/scss/**/*.scss`,
+    html: `${srcFolder}/**/*.html`,
+    images: `${srcFolder}/img/**/*.{jpg,jpeg,png,gif,webp,svg,ico}`,
     files: `${srcFolder}/files/**/*.*`,
   },
   clean: buildFolder,
