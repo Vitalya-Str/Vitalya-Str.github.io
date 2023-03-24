b26f7ba708375e57b1e24e1c71d6e03ef3cf7f24
Vitalya-Str <Strela2789@gmail.com>
2023-03-24 21:02:33 +0600

    burger

--- Logo/src/js/app.js
+++ Logo/src/js/app.js
@@ -1,8 +1,15 @@
 import * as flsFunctions from "./modules/functions.js";
 
-
 flsFunctions.isWebp();
 
-import Swiper, { Navigation, Pagination } from 'swiper';
+import Swiper, { Navigation, Pagination } from "swiper";
 
 const swiper = new Swiper();
+
+const menuBody = document.querySelector(".menu__body");
+const iconMenu = document.querySelector(".icon-menu");
+
+iconMenu.addEventListener("click", () => {
+  menuBody.classList.toggle("_active");
+  iconMenu.classList.toggle("_active");
+});
