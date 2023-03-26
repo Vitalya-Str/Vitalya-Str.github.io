9b4e54f8f6fc24565a229071436a46f43b204151
Vitalya-Str <Strela2789@gmail.com>
2023-03-26 23:10:53 +0600

    end header

--- Logo/src/js/app.js
+++ Logo/src/js/app.js
@@ -8,8 +8,10 @@ const swiper = new Swiper();
 
 const menuBody = document.querySelector(".menu__body");
 const iconMenu = document.querySelector(".icon-menu");
+const bottomHeaderColumn = document.querySelector(".bottom-header__column");
 
 iconMenu.addEventListener("click", () => {
   menuBody.classList.toggle("_active");
   iconMenu.classList.toggle("_active");
+  bottomHeaderColumn.classList.toggle("_active");
 });
