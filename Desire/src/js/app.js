f58541d03a829674e65eff435e77bc72bebcff7c
Vitalya-Str <Strela2789@gmail.com>
2023-04-11 23:03:12 +0600

    rightSideMenu

---
+++ Desire/src/js/app.js
@@ -0,0 +1,12 @@
+
+const headerBtn = document.querySelector(".header__btn");
+const rightsideMenu = document.querySelector(".rightside-menu");
+const rightsideMenuClose = document.querySelector(".rightside-menu__close");
+
+headerBtn.addEventListener("click", function () {
+  rightsideMenu.classList.remove("rightside-menu--close");
+});
+
+rightsideMenuClose.addEventListener("click", function () {
+  rightsideMenu.classList.add("rightside-menu--close");
+});
